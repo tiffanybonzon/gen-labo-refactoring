04be8e9b06efa85216a4bc2cc0b2ba52ce69ceee
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-05-17 23:29:33 +0200

    Long Method - Extract Method createProductDetails

--- src/main/java/OrdersWriter.java
+++ src/main/java/OrdersWriter.java
@@ -18,26 +18,7 @@ public class OrdersWriter {
             for (int j = 0; j < order.getProductsCount(); j++) {
                 Product product = order.getProduct(j);
 
-                sb.append("{");
-                sb.append("\"code\": \"");
-                sb.append(product.getCode());
-                sb.append("\", ");
-                sb.append("\"color\": \"");
-                sb.append(getColorFor(product));
-                sb.append("\", ");
-
-                if (product.getSize() != Size.SIZE_NOT_APPLICABLE) {
-                    sb.append("\"size\": \"");
-                    sb.append(getSizeFor(product));
-                    sb.append("\", ");
-                }
-
-                sb.append("\"price\": ");
-                sb.append(product.getPrice());
-                sb.append(", ");
-                sb.append("\"currency\": \"");
-                sb.append(product.getCurrency());
-                sb.append("\"}, ");
+                createProductDetails(sb, product);
             }
 
             if (order.getProductsCount() > 0) {
@@ -53,6 +34,29 @@ public class OrdersWriter {
         }
 
         return sb.append("]}").toString();
+    }
+
+    private void createProductDetails(StringBuffer sb, Product product) {
+        sb.append("{");
+        sb.append("\"code\": \"");
+        sb.append(product.getCode());
+        sb.append("\", ");
+        sb.append("\"color\": \"");
+        sb.append(getColorFor(product));
+        sb.append("\", ");
+
+        if (product.getSize() != Size.SIZE_NOT_APPLICABLE) {
+            sb.append("\"size\": \"");
+            sb.append(getSizeFor(product));
+            sb.append("\", ");
+        }
+
+        sb.append("\"price\": ");
+        sb.append(product.getPrice());
+        sb.append(", ");
+        sb.append("\"currency\": \"");
+        sb.append(product.getCurrency());
+        sb.append("\"}, ");
     }
 
     private String getSizeFor(Product product) {
